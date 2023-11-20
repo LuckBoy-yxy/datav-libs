b709dc65c2dcc6bb152664c97e5478e2b3efd5a7
yuanxiaoran <hmxs_hmbb@163.com>
2023-11-20 12:13:09 +0800

    svg 外部资源应用已完成

--- dist/datav-libs.js
+++ dist/datav-libs.js
@@ -20,7 +20,85 @@
     height: "0",
     viewBox: "0 0 100 100"
   };
-  const _hoisted_2 = /*#__PURE__*/vue.createStaticVNode("<defs><symbol id=\"more\" viewBox=\"0 0 100 100\"><circle r=\"5\" cx=\"20\" cy=\"25\" fill=\"currentColor\"></circle><circle r=\"5\" cx=\"20\" cy=\"50\" fill=\"currentColor\"></circle><circle r=\"5\" cx=\"20\" cy=\"75\" fill=\"currentColor\"></circle><line x1=\"40\" y1=\"25\" x2=\"90\" y2=\"25\" stroke-width=\"8\" stroke=\"currentColor\"></line><line x1=\"40\" y1=\"50\" x2=\"75\" y2=\"50\" stroke-width=\"8\" stroke=\"currentColor\"></line><line x1=\"40\" y1=\"75\" x2=\"90\" y2=\"75\" stroke-width=\"8\" stroke=\"currentColor\"></line></symbol><symbol id=\"filledArrowRight\" viewBox=\"0 0 100 100\"><polyline points=\"20 10, 80 50, 20 90\" fill=\"currentColor\"></polyline></symbol><symbol id=\"arrowRight\" viewBox=\"0 0 100 100\"><polyline points=\"20 10, 80 50, 20 90, 20 10\" fill=\"transparent\" stroke=\"currentColor\" stroke-width=\"3\"></polyline></symbol></defs>", 1);
+  const _hoisted_2 = /*#__PURE__*/vue.createElementVNode("defs", null, [
+    /*#__PURE__*/vue.createElementVNode("symbol", {
+      id: "more",
+      viewBox: "0 0 100 100"
+    }, [
+      /*#__PURE__*/vue.createElementVNode("circle", {
+        r: "5",
+        cx: "20",
+        cy: "25",
+        fill: "currentColor"
+      }),
+      /*#__PURE__*/vue.createElementVNode("circle", {
+        r: "5",
+        cx: "20",
+        cy: "50",
+        fill: "currentColor"
+      }),
+      /*#__PURE__*/vue.createElementVNode("circle", {
+        r: "5",
+        cx: "20",
+        cy: "75",
+        fill: "currentColor"
+      }),
+      /*#__PURE__*/vue.createElementVNode("line", {
+        x1: "40",
+        y1: "25",
+        x2: "90",
+        y2: "25",
+        "stroke-width": "8",
+        stroke: "currentColor"
+      }),
+      /*#__PURE__*/vue.createElementVNode("line", {
+        x1: "40",
+        y1: "50",
+        x2: "75",
+        y2: "50",
+        "stroke-width": "8",
+        stroke: "currentColor"
+      }),
+      /*#__PURE__*/vue.createElementVNode("line", {
+        x1: "40",
+        y1: "75",
+        x2: "90",
+        y2: "75",
+        "stroke-width": "8",
+        stroke: "currentColor"
+      })
+    ]),
+    /*#__PURE__*/vue.createElementVNode("symbol", {
+      id: "filledArrowRight",
+      viewBox: "0 0 100 100"
+    }, [
+      /*#__PURE__*/vue.createElementVNode("polyline", {
+        points: "20 10, 80 50, 20 90",
+        fill: "currentColor"
+      })
+    ]),
+    /*#__PURE__*/vue.createElementVNode("symbol", {
+      id: "arrowRight",
+      viewBox: "0 0 100 100"
+    }, [
+      /*#__PURE__*/vue.createElementVNode("polyline", {
+        points: "20 10, 80 50, 20 90, 20 10",
+        fill: "transparent",
+        stroke: "currentColor",
+        "stroke-width": "3"
+      })
+    ]),
+    /*#__PURE__*/vue.createElementVNode("symbol", {
+      id: "fitness",
+      viewBox: "0 0 1024 1024"
+    }, [
+      /*#__PURE__*/vue.createElementVNode("path", {
+        d: "M848.6912 238.7456a92.16 92.16 0 0 0-32 5.8368 104.5504 104.5504 0 0 0-208.7424 9.2672v150.9376H421.1712V253.8496a104.5504 104.5504 0 0 0-208.6912-9.2672 92.7744 92.7744 0 0 0-125.184 87.3472v340.3776a92.928 92.928 0 0 0 125.184 87.3472 104.5504 104.5504 0 0 0 208.6912-9.2672v-150.9376h186.7776v150.9376a104.5504 104.5504 0 0 0 208.7424 9.2672 92.16 92.16 0 0 0 32 5.7856A93.2352 93.2352 0 0 0 942.08 672.3072V331.9296a93.2352 93.2352 0 0 0-93.3888-93.184zM180.4288 704a31.6928 31.6928 0 0 1-31.6928-31.6928V331.9296a31.6928 31.6928 0 0 1 63.2832-2.9696v346.3168a31.744 31.744 0 0 1-31.5904 28.7232z m179.2 46.3872a43.1616 43.1616 0 1 1-86.272 0v-75.1104-343.3472-78.08a43.1616 43.1616 0 1 1 86.272 0z m247.9616-212.3776H421.4272v-71.68h186.2656z m104.96 255.5904a43.2128 43.2128 0 0 1-43.1616-43.1616v-496.64a43.1616 43.1616 0 0 1 86.3232 0V750.3872a43.2128 43.2128 0 0 1-43.1616 43.2128zM880.64 672.3072a31.6928 31.6928 0 0 1-63.232 2.8672v-346.112a31.6928 31.6928 0 0 1 63.232 2.8672z",
+        fill: "#F66A2F",
+        "p-id": "1185"
+      })
+    ])
+  ], -1 /* HOISTED */);
   const _hoisted_3 = [
     _hoisted_2
   ];
@@ -120,37 +198,45 @@
   }
 
   //
-  //
-  //
-  //
-  //
-  //
-
   var script$3 = {
     name: 'IconCom',
     props: {
       name: String,
+      prefix: {
+        type: String,
+        "default": 'icon-'
+      },
       style: Object
     },
     setup: function setup(props) {
-      var iconName = "#".concat(props.name);
+      var iconName = vue.computed(function () {
+        return "#".concat(props.prefix).concat(props.name);
+      });
       return {
         iconName: iconName
       };
     }
   };
 
-  const _hoisted_1$3 = ["href"];
+  const _hoisted_1$3 = { class: "icon" };
+  const _hoisted_2$1 = ["href"];
 
   function render$3(_ctx, _cache) {
-    return (vue.openBlock(), vue.createElementBlock("svg", {
+    return (vue.openBlock(), vue.createElementBlock("div", {
+      class: "icon-wrapper",
       style: vue.normalizeStyle({ ..._ctx.style })
     }, [
-      vue.createElementVNode("use", { href: _ctx.iconName }, null, 8 /* PROPS */, _hoisted_1$3)
+      (vue.openBlock(), vue.createElementBlock("svg", _hoisted_1$3, [
+        vue.createElementVNode("use", { href: _ctx.iconName }, null, 8 /* PROPS */, _hoisted_2$1)
+      ]))
     ], 4 /* STYLE */))
   }
 
+  var css_248z$2 = ".icon-wrapper[data-v-38d2d0ef] {\n  display: inline-block;\n}\n\n.icon[data-v-38d2d0ef] {\n  width: 100%;\n  height: 100%;\n  vertical-align: -0.15em;\n  fill: currentColor;\n  overflow: hidden;\n}";
+  styleInject(css_248z$2);
+
   script$3.render = render$3;
+  script$3.__scopeId = "data-v-38d2d0ef";
   script$3.__file = "src/components/Icon/Icon.vue";
 
   function IconCom (Vue) {
--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -1,18 +1,28 @@
 <template>
-  <svg :style="{ ...style }">
-    <use :href="iconName"></use>
-  </svg>
+  <div class="icon-wrapper" :style="{ ...style }">
+    <svg class="icon">
+      <use :href="iconName"></use>
+    </svg>
+  </div>
 </template>
 
 <script>
+  import { computed } from 'vue'
+
   export default {
     name: 'IconCom',
     props: {
       name: String,
-      style: Object
+      prefix: {
+        type: String,
+        default: 'icon-'
+      },
+      style: Object,
     },
     setup(props) {
-      const iconName = `#${ props.name }`
+      const iconName = computed(() => {
+        return `#${ props.prefix }${ props.name }`
+      })
 
       return {
         iconName
@@ -21,6 +31,15 @@
   }
 </script>
 
-<style lang="less" scoped>
-
+<style lang="scss" scoped>
+.icon-wrapper {
+  display: inline-block;
+}
+.icon {
+  width: 100%;
+  height: 100%;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
